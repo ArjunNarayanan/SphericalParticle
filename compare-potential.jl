6a695e98279c11348f6838b5ca31c3f3497323e3
Arjun <arjun.narayanan91@gmail.com>
2021-03-25 09:13:14 -0700

    pepi potential matches mine

--- compare-potential.jl
+++ compare-potential.jl
@@ -44,12 +44,16 @@ function my_potential_difference(
     return (sse - scw) - (cse - ccw)
 end
 
-Ks, Kc = 247e9, 192e9
-ms, mc = 126e9, 87e9
+Ks, Kc = 247., 192.
+ms, mc = 126., 87.
 theta0 = -0.067
 rhos, rhoc = 3.93e3, 3.68e3
 V0s = 1.0 / rhos
 V0c = 1.0 / rhoc
+
+ΔG0Jmol = -14351.0
+molarmass = 0.147
+ΔG0 = ΔG0Jmol/molarmass
 
 outer_radius = 1.0
 dx = outer_radius / 1e3
@@ -80,17 +84,11 @@ mypd =
         V0c,
     )
 
+err = maximum(abs.(mypd - pepipd))
+
+pd = (mypd*1e9 .+ ΔG0)/abs(ΔG0)
+
 fig, ax = PyPlot.subplots()
 ax.plot(inner_radius, pd)
 ax.grid()
 fig
-
-# solver = SP.SphericalSolver(inner_radius,outer_radius,Ks,ms,Kc,mc,theta0)
-#
-# p1 = pressure(SP.core_stress(solver))
-# shelldevstress = deviatoric_stress(SP.shell_stress(solver,inner_radius))
-# srr = shelldevstress[1]
-#
-# t1 = p1*(V0s-V0c)
-# t2 = -0.5*p1^2*(V0s/Ks - V0c/Kc)
-# t3 = 0.5*V0s*(1/Ks+3/4ms)*srr^2
